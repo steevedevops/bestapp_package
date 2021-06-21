4aa1f172f12717905ce2722a3c862bb4be850480
Steeve bernard <steeve@Steeves-MacBook-Pro.local>
2021-06-21 04:57:51 -0300

    Committer: Steeve bernard <steeve@Steeves-MacBook-Pro.local>

--- lib/src/utils/bestapp-utils.dart
+++ lib/src/utils/bestapp-utils.dart
@@ -28,7 +28,7 @@ class BestappUtils {
     }
   }
 
-  static String obterCep(String cep, {bool ponto = false}) {
+  static String formatCep(String cep, {bool ponto = false}) {
     assert(cep.length == 8, 'CEP com tamanho inválido. Deve conter 8 caracteres');
 
     return ponto
@@ -36,19 +36,37 @@ class BestappUtils {
         : '${cep.substring(0, 2)}${cep.substring(2, 5)}-${cep.substring(5, 8)}';
   }
 
+  static String formatTelefone(String telefone, {bool ddd = true}) {
+    if (ddd) {
+      assert((telefone.length == 10 || telefone.length == 11),
+          'Telefone com tamanho inválido. Deve conter 10 ou 11 caracteres');
+
+      return telefone.length == 10
+          ? '(${telefone.substring(0, 2)}) ${telefone.substring(2, 6)}-${telefone.substring(6, 10)}'
+          : '(${telefone.substring(0, 2)}) ${telefone.substring(2, 7)}-${telefone.substring(7, 11)}';
+    } else {
+      assert((telefone.length == 8 || telefone.length == 9),
+          'Telefone com tamanho inválido. Deve conter 8 ou 9 caracteres');
+
+      return (telefone.length == 8)
+          ? '${telefone.substring(0, 4)}-${telefone.substring(4, 8)}'
+          : '${telefone.substring(0, 5)}-${telefone.substring(5, 9)}';
+    }
+  }
+
   /// Retorna o CPF utilizando a máscara: `XXX.YYY.ZZZ-NN`
-  static String obterCpf(String cpf) {
+  static String formatCpf(String cpf) {
     // assert(CPFValidator.isValid(cpf), 'CPF inválido!');
     return CPFValidator.format(cpf);
   }
 
   /// Retorna o CNPJ informado, utilizando a máscara: `XX.YYY.ZZZ/NNNN-SS`
-  static String obterCnpj(String cnpj) {
+  static String formatCnpj(String cnpj) {
     // assert(CNPJValidator.isValid(cnpj), 'CNPJ inválido!');
     return CNPJValidator.format(cnpj);
   }
 
-  static String obterCPFCNPJ(String value) {    
+  static String formatCPFCNPJ(String value) {    
     if(value.length <=11){      
       return CPFValidator.format(value);
     }else{      
